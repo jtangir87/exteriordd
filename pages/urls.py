c26f8b4d739a60a9da11ddcad4f8b3a0f649700a
Jared Tangir <jaredtangir@Jareds-Mac-mini-2.local>
2020-07-28 16:56:22 -0400

    home page updates, contact us created

--- pages/urls.py
+++ pages/urls.py
@@ -19,4 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("contact-us", TemplateView.as_view(template_name="pages/contact_us.html"), name="contact_us"),
 ]
